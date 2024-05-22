701018ef3690258324954f9818ecd0a3af3630c0
franciscaap <155903837+franciscaap@users.noreply.github.com>
2024-05-22 12:24:57 +0100

    set initial loaded page default city to London

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="app-container">
-        <WeatherSearch />
+        <WeatherSearch defaultCity="London" />
       </div>
     </div>
   );
