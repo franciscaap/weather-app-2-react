28af8e9c08f731fddddc890c6c554aed38064518
franciscaap <155903837+franciscaap@users.noreply.github.com>
2024-05-23 14:09:38 +0100

    Made App responsive.

--- src/WeatherDetails.js
+++ src/WeatherDetails.js
@@ -4,44 +4,37 @@ import "./WeatherDetails.css";
 export default function WeatherDetails(props) {
   return (
     <div className="WeatherDetails">
-      <div className="current-weather d-flex justify-content-between">
-        <div>
+      <div className="row mt-2">
+        <div className="col-md-6">
           <h1 className="current-city">{props.info.city}</h1>
           <h3 className="country">{props.info.country}</h3>
           <p className="current-details">
-            <span className="current-time" id="time">
-              Saturday 21:40,
-            </span>
-            <span id="description"> {props.info.description}</span>
+            <span className="current-time">Saturday 21:40,</span>
+            <span> {props.info.description}</span>
             <br />
-            <span id="temperature-details">
-              Humidity: <strong id="humidity">{props.info.humidity}%</strong>,
-              Wind:
-              <strong id="wind"> {props.info.wind}km/h</strong>
+            <span className="temperature-details">
+              Humidity: <strong>{props.info.humidity}%</strong>, Wind:
+              <strong> {props.info.wind}km/h</strong>
             </span>
           </p>
         </div>
-        <div className="current-temperature d-flex">
-          <div>
-            <div id="icon">
-              <img
-                src={props.info.icon}
-                className="current-temperature-icon"
-                alt="Icon"
-              />
+        <div className="col-md-6">
+          <div className="current-temperature">
+            <div className=" d-flex">
+              <div>
+                <div>
+                  <img
+                    src={props.info.icon}
+                    className="current-temperature-icon"
+                    alt="Icon"
+                  />
+                </div>
+              </div>
+              <div className="current-temperature-value fw-bold">
+                {props.info.temperature}
+              </div>
+              <div className="current-temperature-unit">°C</div>
             </div>
-          </div>
-          <div
-            className="current-temperature-value fw-bold"
-            id="current-temperature-value"
-          >
-            {props.info.temperature}
-          </div>
-          <div
-            className="current-temperature-unit"
-            id="current-temperature-unit"
-          >
-            °C
           </div>
         </div>
       </div>
